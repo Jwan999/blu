e2551aa86a2154b1309e864d0d6e5b416cfdf55b
Jwan <jwanaalfatla1999@gmail.com>
2021-03-09 19:52:42 +0300

    First commit

--- resources/views/components /welcoming.blade.php
+++ resources/views/components /welcoming.blade.php
@@ -2,13 +2,16 @@
     <h1 class="lg:text-8xl text-6xl text-center text-white lg:mt-52 mt-20 w-full">
         الحياة بالالوان
     </h1>
-    <div class="lg:w-6/12 w-11/12 ">
+    <div class="lg:w-6/12 w-11/12 flex-col">
         <p class="mt-20 text-white text-justify text-xl leading-relaxed text-center items-center">
             لأننا نطمح دائماً في التميز ، لذا اعتمدنا أطروحات كثيرة في حلول مشاكل الانهاءات الانشائية والديكورية وذلك
             بدمج
             خبرتنا مع احتياجات الزبائن والسوق المحلي العراقي لأننا مؤمنون ما يميزنا هو تنوع منتجاتنا وذلك من خلال توفير
             منتجات من مناشئ عالمية أوربية لكي نساهم في تحسين الطابع المعماري العام في بلدنا العزيز العراق.
         </p>
+        <h1 class="text-base text-gray-200 text-end mt-6">
+            CEO, Ahmed Akram
+        </h1>
     </div>
 
 </div>
